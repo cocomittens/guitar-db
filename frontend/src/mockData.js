5a02d04ec578a5f62d948d531b29845d8ced7d1a
trish <p.stackpole@gmail.com>
2025-05-05 21:32:28 -0700

    Update mockData.js
    Updated to match the sample data from our .sql file

--- frontend/src/mockData.js
+++ frontend/src/mockData.js
@@ -1,38 +1,11 @@
 export const song_data = [
-  {
-    title: "Song 1",
-    _id: 1,
-    artist: "Artist 1",
-    album: "Album 1",
-    key: "C",
-    bpm: 120,
-    capo: 0,
-    difficulty: 1.5,
-    year: 2020,
-  },
-  {
-    title: "Song 2",
-    _id: 2,
-    artist: "Artist 2",
-    album: "Album 2",
-    key: "G",
-    bpm: 130,
-    capo: 2,
-    difficulty: 3,
-    year: 2019,
-  },
+  {song_id: 1, album_id: 7, genre_id: 3, artist_id: 6, title: "Hot August Rain", bpm: 90, difficulty: 0.5, capo: 0, chords: "A, E, D"},
+  {song_id: 2, album_id: 6, genre_id: 2, artist_id: 3, title: "Corona Rumba", bpm: 130, difficulty: 0.2, capo: 3, chords: "D, C, G"},
+  {song_id: 3, album_id: 5, genre_id: 1, artist_id: 1, title: "Metal Mouth", bpm: 130, difficulty: 0.7, capo: 3, chords: "Am, F, C, G"},
 ];
 
 export const song_columns = [
-  "Title",
-  "Song ID",
-  "Artist",
-  "Album",
-  "Key",
-  "BPM",
-  "Capo",
-  "Difficulty",
-  "Release Year",
+  "song_id", "album_id", "genre_id", "artist_id", "title", "bpm", "difficulty", "capo", "chords"
 ];
 
 export const student_data = [
@@ -67,3 +40,35 @@ export const label_data = [
 ];
 
 export const label_columns = ["Label ID", "Label Name"];
+
+export const genre_data = [
+  { _id: 1, name: "Rock" },
+  { _id: 2, name: "Pop" },
+  { _id: 3, name: "County" },
+];
+
+export const genre_columns = ["genre_id", "name"];
+
+export const artist_data = [
+  { _id: 1, name: "Mega Charged" },
+  { _id: 2, name: "Better Than Mega Charged" },
+  { _id: 3, name: "#Hashtag" },
+  { _id: 4, name: "The 340 Mood" },
+  { _id: 5, name: "Vegan Tequila" },
+  { _id: 6, name: "During June" },
+];
+
+export const artist_columns = ["artist_id", "name"];
+
+export const album_data = [
+    {album_id: 1, label_id: 5, artist_id: 3, release_year: "2020", title: "Jager Bomb Blues"},
+    {album_id: 2, label_id: 2, artist_id: 2, release_year: "1997", title: "Still Better Than Mega Charged"},
+    {album_id: 3, label_id: 2, artist_id: 2, release_year: "1999", title: "Now Even Better Than Mega Charged"},
+    {album_id: 4, label_id: 1, artist_id: 1, release_year: "1997", title: "Screaming into the Abyss"},
+    {album_id: 5, label_id: 1, artist_id: 1, release_year: "2017", title: "20 Years of Screaming"},
+    {album_id: 6, label_id: 3, artist_id: 3, release_year: "2021", title: "Now That's What I Call Covid!"},
+    {album_id: 7, label_id: 6, artist_id: 3, release_year: "2024", title: "After July"},
+    {album_id: 8, label_id: 6, artist_id: 3, release_year: "2022", title: "Cold Beer"},
+];
+
+export const album_columns = ["album_id", "artist_id", "label_id", "release_year", "title"];
